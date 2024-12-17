3fa3ba59a84827b78bd01e7d2c499d15d2763cac
Hoang381202 <phuonglien3802@gmail.com>
2024-12-17 19:08:55 +0700

    Add files via upload

---
+++ cutchanhubv6.lua
@@ -0,0 +1,3 @@
+getgenv().Key = "MARU-A8U1I-L9TR-G7KEA-OJRT-1PXIA"
+getgenv().id = "1210922522435133471"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
